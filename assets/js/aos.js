39e6180811903b88caa9cdeb176ddbbbe067398c
Guilherme Ferreira <devguilhermeferreira@gmail.com>
2023-03-03 19:24:56 -0300

    estrutura feita

---
+++ assets/js/aos.js
@@ -0,0 +1,6 @@
+// AOS - Animation Scroll
+export default function initAOS(duration=1000) {
+   AOS.init({
+      duration: duration,
+   });
+}
---
+++ assets/js/main.js
@@ -0,0 +1,2 @@
+import initAOS from "./aos.js";
+initAOS(3000); // 1.5 segundos
