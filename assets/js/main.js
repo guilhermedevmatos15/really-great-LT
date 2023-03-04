1dfd29382f8ea1ed7fc88be94e9fda196e1627e9
Guilherme Ferreira <devguilhermeferreira@gmail.com>
2023-03-04 07:26:57 -0300

    add modal interativo

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,2 +1,16 @@
 import initAOS from "./aos.js";
-initAOS(3000); // 1.5 segundos
+initAOS(1700); // 1.5 segundos
+
+const links = [...document.querySelectorAll('main .links > .link')];
+links.forEach((element, chave, array)=>{
+   element.addEventListener('click', (e)=>{
+      const nameLink = e.target.querySelector('p').innerHTML;
+      const modal = document.querySelector('div.modal');
+      
+      modal.classList.add('active');
+      modal.querySelector('p.name-link').innerHTML=nameLink;
+      modal.addEventListener('click', ()=>{
+         modal.classList.remove('active');
+      });
+   });
+})
